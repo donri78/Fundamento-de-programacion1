8d584491d1158d2b1aad43bb9d9a2c77260806cf
donri78 <richar.molina.monaga@utelvt.edu.ec>
2021-12-16 18:10:29 -0500

    elimine archivo basura

--- ejemplo.sh
+++
@@ -1,3 +0,0 @@
-git clone https://emely500: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@github.com/emely500/Fundamento-de-programacion1.git
-
-git clone https://AngelicaQuinter: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@github.com/AngelicaQuinter/Fundamento-de-programacion1.git
