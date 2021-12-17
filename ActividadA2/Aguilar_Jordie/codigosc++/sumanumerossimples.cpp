fcb601784cd67790be6a5baa437a76a77d81d5bb
donri78 <richar.molina.monaga@utelvt.edu.ec>
2021-12-17 18:30:19 -0500

    nada

---
+++ ActividadA2/Aguilar_Jordie/codigosc++/sumanumerossimples.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+int main()
+{
+float x,y,z;
+cout<<"ingrese el numero x : ";  cin>>x;
+
+cout<<"ingrese el numero y : ";  cin>>y;
+z=x+y;
+
+cout<<"el valor de la suma es | "<<z<<endl;
+
+return  0;
+
+}
