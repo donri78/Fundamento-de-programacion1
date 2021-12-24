d7d3f5d86a06eace6d47559255fd5a0c1e5e1906
Rommel Ortiz Napa <86314590+Jeremio8@users.noreply.github.com>
2021-12-24 13:10:25 -0500

    Actividad C2

---
+++ ACTIVIDAD-C2/Ortiz Jeremy/SUMA DE DOS NUMEROS.h
@@ -0,0 +1,16 @@
+# include <iostream>
+
+using namespace std;
+int main()
+{
+float a,b,c;
+cout<<"ingrese el numero a : ";  cin>>a;
+
+cout<<"ingrese el numero b : ";  cin>>b;
+c=a+b;
+
+cout<<"el valor de la suma es | "<<c<<endl;
+
+return  0;
+
+}
