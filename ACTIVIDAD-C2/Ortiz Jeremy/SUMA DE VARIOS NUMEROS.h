ab916683a06be5e4f12dba931245d0f5b8462494
Rommel Ortiz Napa <86314590+Jeremio8@users.noreply.github.com>
2021-12-24 13:40:48 -0500

    Actividad C2

---
+++ ACTIVIDAD-C2/Ortiz Jeremy/SUMA DE VARIOS NUMEROS.h
@@ -0,0 +1,19 @@
+# include <iostream>
+
+using namespace std;
+int main()
+{
+
+        int c=0,n;
+        float s=0,x;                                    cout<<"ingrese el valor de la suma: ";  cin>>n;
+        do{
+                                                       cout<<"ingrese el elemento : "<<c+1;
+       cin>>x;
+       c=c+1;
+       s=s+x;
+        }while(c<n);
+                                                cout<<"la suma de los elementos fue : "<<s<<endl;
+
+return  0;
+
+}
